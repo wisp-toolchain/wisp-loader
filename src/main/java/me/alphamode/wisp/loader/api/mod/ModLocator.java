0d9eaad6b8a38ab33062023d60f4871e59f808ac
alpha <alphamodeyt@gmail.com>
2025-12-30 21:13:15 -0600

    Mark ModLocator deprecated for removal

--- src/main/java/me/alphamode/wisp/loader/api/mod/ModLocator.java
+++ src/main/java/me/alphamode/wisp/loader/api/mod/ModLocator.java
@@ -1,8 +1,14 @@
 package me.alphamode.wisp.loader.api.mod;
 
+import me.alphamode.wisp.loader.api.plugin.PluginContext;
+
 import java.nio.file.Path;
 import java.util.List;
 
+/**
+ * Mods should be loaded through {@link PluginContext#getBuildingModList()} during the init phase
+ */
+@Deprecated(forRemoval = true)
 public interface ModLocator {
     List<Path> locateMods();
 }
